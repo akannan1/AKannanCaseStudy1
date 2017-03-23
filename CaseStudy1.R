f4b6973d3b3fec154ca827125150a70ab5c09d34
akannan1 <akannan@smu.edu>
2017-03-23 18:17:36 -0400

    Add files via upload

--- CaseStudy1.R
+++ CaseStudy1.R
@@ -1,3 +1,5 @@
+##Downloading GDP
+
 if (!file.exists("data")){
   dir.create("data")
 }
@@ -5,17 +7,18 @@ fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 download.file(fileURL,destfile="C:/Users/user/Desktop/gdp.csv", method='curl')
 gdp <- read.csv("C:/Users/user/Desktop/gdp.csv",skip=5,header=FALSE,nrows=200)
 
-
+##Column Names
 colnames(gdp)[1]="CountryCode"
 colnames(gdp)[5]="gdp"
 gdp$gdp <- as.numeric(gsub(",","",gdp$gdp))
 
+##Download Education File
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
 download.file(fileURL,destfile="C:/Users/user/Desktop/gdp.csv", method='curl')
 dateDownloaded <- date()
 education <- read.csv("C:/Users/user/Desktop/Education.csv",header=TRUE)
 
-
+##Merge/Sort/Quantile Data
 FullData <- merge(gdp,education,by="CountryCode")
 FullDataSorted <- FullData[order(FullData$gdp,na.last=NA),]
 FullData$quantile <- cut(FullDataSorted$V2,5)
